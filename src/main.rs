ee3a7424ce4fa53f3fd8b0667c1542914e717ccd
Shrey Kavi <shrey.kavi@gmail.com>
2022-03-11 14:02:39 -0500

    Setup turn based 'X' and 'O'
    Uses string and knows which one to set next

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 use std::io;
 
 pub struct Grid {
-    grid: Vec<Vec<i32>>,
+    grid: Vec<Vec<String>>,
 }
 
 impl Grid {
     pub fn new(m: usize) -> Grid {
-        let mut grid = vec![vec![0; m]; m];
+        let mut grid = vec![vec![String::from("_"); m]; m];
         Grid { grid }
     }
 
-    pub fn set_position(&mut self, x: usize, y: usize, marking: i32) {
+    pub fn set_position(&mut self, x: usize, y: usize, marking: String) {
         // if not set, set the position
         self.grid[y][x] = marking;
     }
 
     pub fn print(&self) {
         // if not set, set the position
-        println!("{:?}", self.grid);
+        for row in &self.grid {
+            println!("{:?}", row);
+        }
     }
 }
 
@@ -33,9 +35,14 @@ fn main() {
     // Create grid
     let mut grid = Grid::new(size.trim().parse().expect("Expected an int!"));
 
+    // players 1 and -2
+    let mut next_player = "X";
+
     // Loop turn by turn to get next move
     // Stop loop when either wins or theres a draw
     loop {
+        next_player = if next_player == "O" { "X" } else { "O" };
+        println!("Player {}'s turn", next_player);
         println!("Input the next move");
 
         let mut next_move = String::new();
@@ -43,7 +50,7 @@ fn main() {
             .read_line(&mut next_move)
             .expect("No move specified!");
 
-        grid.set_position(0, 0, 1);
+        grid.set_position(0, 0, String::from(next_player));
         grid.print();
     }
 }
