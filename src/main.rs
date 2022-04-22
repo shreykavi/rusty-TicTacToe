be3ee5fd48e544a7f7a272a0695b881c86396f13
Shrey Kavi <shrey.kavi@gmail.com>
2022-04-22 16:26:43 -0400

    Check if game has ended at each move

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ impl Grid {
         Grid { grid, size: m }
     }
 
-    pub fn set_position(&mut self, mov: Move, marking: String) -> bool {
+    pub fn set_position(&mut self, mov: &Move, marking: String) -> bool {
         // TODO: error check the request
         if mov.x >= self.size || mov.y >= self.size || self.grid[mov.y][mov.x] != "_" {
             println!("This is an incorrect move.");
@@ -22,6 +22,29 @@ impl Grid {
         // if not set, set the position
         self.grid[mov.y][mov.x] = marking;
         return true;
+    }
+
+    pub fn check_game(&mut self, mov: &Move, marking: String) -> bool {
+        // check vertical, horizontal, and 2 diagonals of x,y
+
+        let answer = vec![&marking; self.size];
+        let mut horizontal: Vec<&String> = Vec::new();
+        let mut vertical: Vec<&String> = Vec::new();
+        let mut diagonal1: Vec<&String> = Vec::new();
+        let mut diagonal2: Vec<&String> = Vec::new();
+
+        for coordinate in 0..self.size {
+            horizontal.push(&self.grid[mov.y][coordinate]);
+            vertical.push(&self.grid[coordinate][mov.x]);
+            diagonal1.push(&self.grid[coordinate][coordinate]);
+            diagonal2.push(&self.grid[coordinate][self.size - 1 - coordinate]);
+        }
+
+        if vertical == answer || horizontal == answer || diagonal1 == answer || diagonal2 == answer
+        {
+            return true;
+        }
+        return false;
     }
 
     pub fn print(&self) {
@@ -67,9 +90,11 @@ fn main() {
     // players X and O
     let mut next_player = "X";
 
+    let mut winner = false;
+
     // Loop turn by turn to get next move
     // Stop loop when either wins or theres a draw
-    loop {
+    while !winner {
         next_player = if next_player == "O" { "X" } else { "O" };
         println!("Player {}'s turn", next_player);
         println!("Input the next move (in format 'x,y')");
@@ -83,9 +108,10 @@ fn main() {
                 .expect("No move specified!");
 
             let parsed_move = Move::new(next_move);
-            request = grid.set_position(parsed_move, String::from(next_player));
+            request = grid.set_position(&parsed_move, String::from(next_player));
+            winner = grid.check_game(&parsed_move, String::from(next_player));
         }
-
         grid.print();
     }
+    println!("Player {} has won the game!", next_player);
 }
