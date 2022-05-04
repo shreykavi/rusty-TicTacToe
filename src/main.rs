eb4d92b623db09c24d5e077ddbce67563f0e33a0
Shrey Kavi <shrey.kavi@gmail.com>
2022-05-04 03:41:54 -0400

    Adds states for menu, cleanup, setup and ingame

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,18 @@ const GRID_SPRITE: &str = "grid.png";
 const X_SPRITE: &str = "x.png";
 const O_SPRITE: &str = "o.png";
 const GRID_SIZE: usize = 3; //defaults to grid size 3 // TODO: variable grid size
-const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum AppState {
+    InGame,
+    Winner,
+}
+
+#[derive(Component)]
+struct Menu;
+
+#[derive(Component)]
+struct Marks;
 
 // Resources:
 struct NextPlayer {
@@ -94,6 +105,8 @@ pub struct Move {
 
 fn main() {
     App::new()
+        // add the app state type
+        .add_state(AppState::InGame)
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .insert_resource(WindowDescriptor {
             title: "Rusty Tic Tac Toe".to_string(),
@@ -106,11 +119,16 @@ fn main() {
         .insert_resource(WinitSettings::desktop_app())
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(handle_mouse_clicks)
+        .add_system_set(SystemSet::on_enter(AppState::InGame).with_system(setup))
+        .add_system_set(SystemSet::on_update(AppState::InGame).with_system(handle_mouse_clicks))
+        .add_system_set(SystemSet::on_enter(AppState::Winner).with_system(open_menu_system))
+        .add_system_set(SystemSet::on_update(AppState::Winner).with_system(button_system))
+        .add_system_set(SystemSet::on_exit(AppState::Winner).with_system(restart_system))
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut windows: ResMut<Windows>) {
+    println!("setup");
     // Watches for changes in files
     asset_server.watch_for_changes().unwrap();
 
@@ -151,6 +169,7 @@ fn handle_mouse_clicks(
     win_size: Res<WinSize>,
     mut grid: ResMut<Grid>,
     mut next_player: ResMut<NextPlayer>,
+    mut app_state: ResMut<State<AppState>>,
 ) {
     let win = windows.get_primary().expect("no primary window");
     let separator_width = win_size.w as usize / GRID_SIZE;
@@ -158,114 +177,8 @@ fn handle_mouse_clicks(
     let mut request = false;
     let mut winner = false;
 
-    // For testing
-    if mouse_input.just_pressed(MouseButton::Right) {
-        commands
-            .spawn_bundle(NodeBundle {
-                style: Style {
-                    size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                    justify_content: JustifyContent::SpaceBetween,
-                    ..default()
-                },
-                color: Color::NONE.into(),
-                ..default()
-            })
-            .with_children(|parent| {
-                // absolute positioning
-                parent
-                    .spawn_bundle(NodeBundle {
-                        style: Style {
-                            size: Size::new(Val::Px(400.0), Val::Px(300.0)),
-                            position_type: PositionType::Absolute,
-                            position: Rect {
-                                left: Val::Px(win_size.w as f32 / 2. - 200.),
-                                bottom: Val::Px(win_size.h as f32 / 2. - 150.),
-                                ..default()
-                            },
-                            border: Rect::all(Val::Px(10.0)),
-                            ..default()
-                        },
-                        color: Color::rgb(0.4, 0.4, 1.0).into(),
-                        ..default()
-                    })
-                    .with_children(|parent| {
-                        parent
-                            .spawn_bundle(NodeBundle {
-                                style: Style {
-                                    size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                                    flex_direction: FlexDirection::ColumnReverse,
-                                    justify_content: JustifyContent::Center,
-                                    align_items: AlignItems::Center,
-                                    align_content: AlignContent::Center,
-                                    ..default()
-                                },
-                                color: Color::rgb(0.8, 0.8, 1.0).into(),
-                                ..default()
-                            })
-                            .with_children(|parent| {
-                                parent.spawn_bundle(TextBundle {
-                                    style: Style {
-                                        // center button
-                                        margin: Rect::all(Val::Auto),
-                                        // horizontally center child text
-                                        justify_content: JustifyContent::Center,
-                                        // vertically center child text
-                                        align_items: AlignItems::Center,
-                                        align_content: AlignContent::Center,
-                                        max_size: Size::new(Val::Px(350.0), Val::Px(100.0)),
-                                        ..default()
-                                    },
-                                    text: Text::with_section(
-                                        "Player X wins!",
-                                        TextStyle {
-                                            font: asset_server.load("fonts/FiraMono-Medium.ttf"),
-                                            font_size: 40.0,
-                                            color: Color::rgb(0.9, 0.9, 0.9),
-                                        },
-                                        TextAlignment {
-                                            vertical: VerticalAlign::Center,
-                                            horizontal: HorizontalAlign::Center,
-                                        },
-                                        // Default::default(),
-                                    ),
-                                    ..default()
-                                });
-                                parent
-                                    .spawn_bundle(ButtonBundle {
-                                        style: Style {
-                                            size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-                                            // center button
-                                            margin: Rect::all(Val::Auto),
-                                            // horizontally center child text
-                                            justify_content: JustifyContent::Center,
-                                            // vertically center child text
-                                            align_items: AlignItems::Center,
-                                            ..default()
-                                        },
-                                        color: NORMAL_BUTTON.into(),
-                                        ..default()
-                                    })
-                                    .with_children(|parent| {
-                                        parent.spawn_bundle(TextBundle {
-                                            text: Text::with_section(
-                                                "Restart",
-                                                TextStyle {
-                                                    font: asset_server
-                                                        .load("fonts/FiraSans-Bold.ttf"),
-                                                    font_size: 40.0,
-                                                    color: Color::rgb(0.9, 0.9, 0.9),
-                                                },
-                                                Default::default(),
-                                            ),
-                                            ..default()
-                                        });
-                                    });
-                            });
-                    });
-            });
-    }
-
     if mouse_input.just_pressed(MouseButton::Left) {
+        println!("handled");
         let click_position = win.cursor_position().unwrap();
         let x = click_position[0] as usize / separator_width;
         let y = click_position[1] as usize / separator_width;
@@ -275,29 +188,196 @@ fn handle_mouse_clicks(
         winner = grid.check_game(&parsed_move, &next_player.mark);
 
         if winner {
-            // TODO: popup done game
-
+            // Change game state
+            app_state.set(AppState::Winner).unwrap();
             println!("Player {} has won the game!", next_player.mark);
         }
         if request {
             // Draw mark
-            let sprite = commands.spawn_bundle(SpriteBundle {
-                transform: Transform {
-                    // x,y,z
-                    translation: Vec3::new(
-                        // - GRID_SIZE/2 on x and y to offset axis since Transforms have 0,0 at center
-                        (separator_width as f32 * (x as f32 - (GRID_SIZE / 2) as f32)),
-                        (separator_width as f32 * (y as f32 - (GRID_SIZE / 2) as f32)),
-                        1.,
-                    ),
-                    scale: Vec3::new(0.25, 0.25, 1.),
+            let sprite = commands
+                .spawn_bundle(SpriteBundle {
+                    transform: Transform {
+                        // x,y,z
+                        translation: Vec3::new(
+                            // - GRID_SIZE/2 on x and y to offset axis since Transforms have 0,0 at center
+                            (separator_width as f32 * (x as f32 - (GRID_SIZE / 2) as f32)),
+                            (separator_width as f32 * (y as f32 - (GRID_SIZE / 2) as f32)),
+                            1.,
+                        ),
+                        scale: Vec3::new(0.25, 0.25, 1.),
+                        ..Default::default()
+                    },
+                    texture: asset_server.load(next_player.sprite()),
                     ..Default::default()
-                },
-                texture: asset_server.load(next_player.sprite()),
-                ..Default::default()
-            });
+                })
+                .insert(Marks);
 
             next_player.switch();
+        }
+    }
+}
+
+fn open_menu_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    win_size: Res<WinSize>,
+    mut next_player: ResMut<NextPlayer>,
+    mut app_state: ResMut<State<AppState>>,
+) {
+    let winner_text = format!("Player {} wins!", next_player.mark);
+    commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                justify_content: JustifyContent::SpaceBetween,
+                ..default()
+            },
+            color: Color::NONE.into(),
+            ..default()
+        })
+        .insert(Menu)
+        .with_children(|parent| {
+            // absolute positioning
+            parent
+                .spawn_bundle(NodeBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(400.0), Val::Px(300.0)),
+                        position_type: PositionType::Absolute,
+                        position: Rect {
+                            left: Val::Px(win_size.w as f32 / 2. - 200.),
+                            bottom: Val::Px(win_size.h as f32 / 2. - 150.),
+                            ..default()
+                        },
+                        border: Rect::all(Val::Px(10.0)),
+                        ..default()
+                    },
+                    color: Color::rgb(0.4, 0.4, 1.0).into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent
+                        .spawn_bundle(NodeBundle {
+                            style: Style {
+                                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                                flex_direction: FlexDirection::ColumnReverse,
+                                justify_content: JustifyContent::Center,
+                                align_items: AlignItems::Center,
+                                align_content: AlignContent::Center,
+                                ..default()
+                            },
+                            color: Color::rgb(0.8, 0.8, 1.0).into(),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            parent.spawn_bundle(TextBundle {
+                                style: Style {
+                                    // center button
+                                    margin: Rect::all(Val::Auto),
+                                    // horizontally center child text
+                                    justify_content: JustifyContent::Center,
+                                    // vertically center child text
+                                    align_items: AlignItems::Center,
+                                    align_content: AlignContent::Center,
+                                    max_size: Size::new(Val::Px(350.0), Val::Px(100.0)),
+                                    ..default()
+                                },
+                                text: Text::with_section(
+                                    winner_text,
+                                    TextStyle {
+                                        font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+                                        font_size: 40.0,
+                                        color: Color::rgb(0.9, 0.9, 0.9),
+                                    },
+                                    TextAlignment {
+                                        vertical: VerticalAlign::Center,
+                                        horizontal: HorizontalAlign::Center,
+                                    },
+                                    // Default::default(),
+                                ),
+                                ..default()
+                            });
+                            parent
+                                .spawn_bundle(ButtonBundle {
+                                    style: Style {
+                                        size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                                        // center button
+                                        margin: Rect::all(Val::Auto),
+                                        // horizontally center child text
+                                        justify_content: JustifyContent::Center,
+                                        // vertically center child text
+                                        align_items: AlignItems::Center,
+                                        ..default()
+                                    },
+                                    color: NORMAL_BUTTON.into(),
+                                    ..default()
+                                })
+                                .with_children(|parent| {
+                                    parent.spawn_bundle(TextBundle {
+                                        text: Text::with_section(
+                                            "Restart",
+                                            TextStyle {
+                                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                                font_size: 40.0,
+                                                color: Color::rgb(0.9, 0.9, 0.9),
+                                            },
+                                            Default::default(),
+                                        ),
+                                        ..default()
+                                    });
+                                });
+                        });
+                });
+        });
+}
+
+fn restart_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    win_size: Res<WinSize>,
+    mut next_player: ResMut<NextPlayer>,
+    mut app_state: ResMut<State<AppState>>,
+    mut menu_query: Query<Entity, (With<Menu>)>,
+    mut mark_query: Query<Entity, (With<Marks>)>,
+) {
+    // close menu
+    for menu in menu_query.iter_mut() {
+        commands.entity(menu).despawn_recursive();
+    }
+
+    // erase marks
+    for mark in mark_query.iter_mut() {
+        commands.entity(mark).despawn();
+    }
+}
+
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor, &Children),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut text_query: Query<&mut Text>,
+    mut app_state: ResMut<State<AppState>>,
+    mut mouse_input: ResMut<Input<MouseButton>>,
+) {
+    for (interaction, mut color, children) in interaction_query.iter_mut() {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Clicked => {
+                *color = PRESSED_BUTTON.into();
+                // Must reset click otherwise counts towards game
+                mouse_input.reset(MouseButton::Left);
+                app_state.set(AppState::InGame).unwrap();
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+            }
         }
     }
 }
